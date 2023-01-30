f7f598f80fb34a7ee4341a1a9dd105ee10148ba2
Nemanja Malesija <malesija13@gmail.com>
2023-01-30 11:47:23 +0100

    Structure SingleProduct component, add props type

--- src/SingleProduct.tsx
+++ src/SingleProduct.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useGlobalContext } from './context';
 
-const SingleProduct = () => {
-  return <div>SingleProduct</div>;
+type singleProductProps = {
+  name: string;
+  description: string;
+  price: number;
+  id: string;
+};
+
+const SingleProduct = (props: singleProductProps) => {
+  const { name, description, price, id } = props;
+  const [amount, setAmount] = useState(1);
+
+  return (
+    <article>
+      <div className="item-side-left">
+        <h5 className="heading-fifth">{name}</h5>
+        <p className="item-description">{description}</p>
+        <p className="item-price">{price}$</p>
+      </div>
+      <div className="item-side-right">
+        <div className="item-input-label">
+          <label htmlFor="text">Amount</label>
+          <input
+            name="amount"
+            type="number"
+            min="1"
+            max="99"
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+          />
+        </div>
+        <div className="div-btn">
+          <button className="btn btn-add">+Add</button>
+        </div>
+      </div>
+    </article>
+  );
 };
 
 export default SingleProduct;
